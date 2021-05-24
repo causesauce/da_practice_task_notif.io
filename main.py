d649bb06939a935d4bda6412cfff8a17a7a09d3e
Adrian Chervinchuk <s20357@pjwstk.edu.pl>
2021-05-24 23:23:04 +0200

    code part done

--- main.py
+++ main.py
@@ -1,1 +1,80 @@
-print("Hello worlds")
+from fastapi import Security, Depends, FastAPI, HTTPException
+from fastapi.security.api_key import APIKeyQuery, APIKeyHeader, APIKey
+from pydantic import PositiveInt
+from starlette.status import HTTP_401_UNAUTHORIZED, HTTP_201_CREATED, HTTP_204_NO_CONTENT, \
+    HTTP_400_BAD_REQUEST, HTTP_200_OK
+from tortoise.contrib.fastapi import register_tortoise
+
+from models import MessageInputValidated, Message, Message_Pydantic, MessageInput, get_all, get_by_id, add_message, \
+    update_message, delete_message
+
+app = FastAPI()
+
+
+API_KEY = "ed049313-16eb-4fc1-aa36-398d21255f76"
+API_KEY_NAME = "access_token"
+
+api_key_query = APIKeyQuery(name=API_KEY_NAME, auto_error=False)
+api_key_header = APIKeyHeader(name=API_KEY_NAME, auto_error=False)
+
+
+async def get_api_key(
+        api_key_query: str = Security(api_key_query),
+        api_key_header: str = Security(api_key_header)
+):
+    if api_key_query == API_KEY:
+        return api_key_query
+    elif api_key_header == API_KEY:
+        return api_key_header
+    else:
+        raise HTTPException(
+            status_code=HTTP_401_UNAUTHORIZED, detail="Wrong credentials"
+        )
+
+
+@app.get("/messages", status_code=HTTP_200_OK)
+async def get_messages():
+    response_list = await get_all()
+    return response_list
+
+
+@app.get("/messages/{id_message}", status_code=HTTP_200_OK)
+async def get_messages_by_id(id_message: PositiveInt):
+    response = await get_by_id(id_message)
+    return response
+
+
+@app.post("/messages", status_code=HTTP_201_CREATED)
+async def post_message_endpoint(message_input: MessageInput = None, api_key: APIKey = Depends(get_api_key)):
+    if message_input is None:
+        raise HTTPException(status_code=HTTP_400_BAD_REQUEST, detail="body should not be empty")
+    message_obj = await add_message(message_input)
+    return f'message with id {message_obj.id_message} has been created'
+
+
+@app.put("/messages/{id_message}")
+async def update_message_endpoint(
+        id_message: PositiveInt,
+        message_input: MessageInput = None,
+        api_key: APIKey = Depends(get_api_key)
+):
+    if message_input is None:
+        raise HTTPException(status_code=HTTP_400_BAD_REQUEST, detail="body should not be empty")
+    message = await update_message(message_input, id_message)
+    return {"detail": f"message with the id {id_message} has been modified",
+            "message": message}
+
+
+@app.delete("/messages/{id_message}", status_code=200)
+async def delete_message_endpoint(id_message: PositiveInt, api_key: APIKey = Depends(get_api_key)):
+    await delete_message(id_message)
+    return {"details": "success"}
+
+
+register_tortoise(
+    app,
+    db_url='sqlite://db.sqlite3',
+    modules={'models': ['main']},
+    generate_schemas=True,
+    add_exception_handlers=True
+)
---
+++ models.py
@@ -0,0 +1,87 @@
+from typing import Optional
+
+from fastapi import HTTPException, FastAPI
+from pydantic import BaseModel, constr, PositiveInt, validator
+from tortoise import Model, fields
+from passlib.hash import bcrypt
+from tortoise.contrib.pydantic import pydantic_model_creator
+
+ZERO_MESSAGE_COUNTER = 0
+
+
+class MessageInput(BaseModel):
+    body: Optional[str]
+
+
+class MessageInputValidated(BaseModel):
+    body: str
+
+    @validator('body', pre=True)
+    def validate_body(cls, body):
+        if body is None or len(body) > 160 or len(body) < 1:
+            raise HTTPException(status_code=400, detail="body should not be empty")
+        else:
+            return body
+
+
+class MessageResponse(BaseModel):
+    body: Optional[constr(min_length=1, max_length=160)]
+    counter: PositiveInt
+
+
+class Message(Model):
+    id_message = fields.IntField(pk=True)
+    body = fields.CharField(160)
+    counter = fields.IntField(pk=False)
+
+    def __str__(self):
+        return self.id_message
+
+    async def increase_counter(self):
+        self.counter += 1
+        await self.save()
+
+    async def reset_counter(self):
+        self.counter = 0
+        await self.save()
+
+
+Message_Pydantic = pydantic_model_creator(Message, name="MessageOut", exclude=('id_message',))
+
+
+async def delete_message(id_message: int):
+    message = await Message.get(id_message=id_message)
+    await message.delete()
+
+
+async def add_message(message_input: MessageInput):
+    message_input_model = MessageInputValidated.parse_obj(dict(body=message_input.body))
+    message_obj = Message(body=message_input_model.body, counter=ZERO_MESSAGE_COUNTER)
+    await message_obj.save()
+    return message_obj
+
+
+async def update_message(message_input: MessageInput, id_message: int):
+    message_input_model = MessageInputValidated.parse_obj(dict(body=message_input.body))
+    message = await Message.get(id_message=id_message)
+    message.body = message_input_model.body
+    message.counter = ZERO_MESSAGE_COUNTER
+    await message.save()
+    message = await Message_Pydantic.from_tortoise_orm(message)
+    return message
+
+
+async def get_all():
+    messages = await Message.all()
+    response = list()
+    for message in messages:
+        await message.increase_counter()
+        response.append(await Message_Pydantic.from_tortoise_orm(message))
+    return response
+
+
+async def get_by_id(id_message: int):
+    message = await Message.get(id_message=id_message)
+    await message.increase_counter()
+    response = await Message_Pydantic.from_tortoise_orm(message)
+    return response
